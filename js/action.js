950e3155c8c1d1217aaaff230fc33942bbd67f7b
Hien Le Minh <lethiminhhienlight10@gmail.com>
2022-03-13 12:12:34 +0700

    test

---
+++ js/action.js
@@ -0,0 +1,10 @@
+const name_user_input = document.getElementById('name-user')
+const phone_number_input = document.getElementById('phone-number')
+const email_input = document.getElementById('email')
+const choice = document.getElementsByName('choice')
+
+
+
+function handle_form_submission() {
+  alert ('Bạn đã đăng ký thành công!')
+}
